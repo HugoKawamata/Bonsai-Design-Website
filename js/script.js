9ad898b912aa4a7a8f14e5c88d1f96e1f111d4de
Hugo Kawamata <hugokawamata@gmail.com>
2016-10-03 01:40:03 +1000

    Jquery animations

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+    "use strict";
+    $(window).scroll(function () {
+        
+        /* Check the location of each desired element */
+        $('.info-cont').each(function (i) {
+            var bottom_of_object = $(this).position().top + $(this).outerHeight(),
+                bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object - 150) {
+                $(this).animate({
+                    'opacity': '1',
+                    'margin-left': '20%'
+                }, 500);
+            }
+        });
+    });
+});
