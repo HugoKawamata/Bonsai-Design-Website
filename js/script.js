d9f9ddd10df4a4fa7b70fe3b738129d18f91f127
Hugo Kawamata <hugokawamata@gmail.com>
2016-10-03 13:35:39 +1000

    Mobile css

--- js/script.js
+++ js/script.js
@@ -9,13 +9,20 @@ $(document).ready(function () {
         $('.info-cont').each(function (i) {
             var bottom_of_object = $(this).position().top + $(this).outerHeight(),
                 bottom_of_window = $(window).scrollTop() + $(window).height();
+                
 
-            /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > (bottom_of_object + $(this).position().top) / 2) {
+            /* If the object is completely visible in the window, fade it in */
+            if (bottom_of_window > (bottom_of_object + $(this).position().top) / 2 && $(window).width() >= 640) {
                 $(this).animate({
                     'opacity': '1',
                     'margin-left': '20%',
                     'margin-right': '20%'
+                }, 500);
+            }
+            
+            if (bottom_of_window > (bottom_of_object + $(this).position().top) / 2 && $(window).width() < 640) {
+                $(this).animate({
+                    'opacity': '1'
                 }, 500);
             }
         });
