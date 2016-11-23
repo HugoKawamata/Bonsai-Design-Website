2f4c4dcfaa388d9ee3d463feb7b726516a89ca70
Michael Moore <michaelmoore1997@gmail.com>
2016-11-23 11:27:59 +1000

    modified stripe files, added htaccess to hide keys

--- resources/phpfiles/config.php
+++ resources/phpfiles/config.php
@@ -2,7 +2,7 @@
 require_once('vendor/autoload.php');
 
 $stripe = array(
-  "secret_key"      => "sk_test_lolthoughtyoucouldgetmykey",
+  "secret_key"      => "sk_test_SBwBIljW9wvtJAImPYIzkGpA",
   "publishable_key" => "pk_test_miqlxHjy5qWGU2MaWevcm3ok"
 );
 
